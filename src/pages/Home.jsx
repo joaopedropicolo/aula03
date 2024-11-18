1239759a51fc6cf647949987a99080091c65370f
joaopedropicolo <bigwon132@gmail.com>
2024-11-18 13:58:54 -0400

    Feito filtros de letras

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,16 +11,32 @@ const [Lista, setLista] = useState([]);
                 const dados = await resposta.json();
                 setLista(dados)
             } catch (erro) {
-                console.error("Erro na API! Produtos não serão exibidos.");
+                console.error("Erro na API! Produtos não exibidos.");
                 alert('Erro na API!')
             }
         }
         receberListaProduto()
     }, []);
 
+const ordeAz = () =>{
+    const listAux = [...Lista].sort((a, b) => a.title.localeCompare(b.title));
+    setLista(listAux);
+}
+
+const ordeZa = () => {
+    const listAux = [...Lista].sort((a, b) => b.title.localeCompare(a.title));
+    setLista(listAux);
+};
+
+    if (Lista.length === 0){
+        return <h1>Carregando...</h1>
+    }
+
 return (
     <>
     <h1>Lista de Produtos</h1>
+    <button onClick={() => ordeAz()}>A-Z</button>
+    <button onClick={() => ordeZa()}>Z-A</button>
     <ListaProdutos Lista={Lista}/>
     </>
 )}
